f1614445bae0c0dcd0e4935764daf61193329d8c
wogh0 <wogh0@DESKTOP-OS4S3HI>
2021-11-27 19:40:37 +0900

    java1 수업 11강

---
+++ HelloWorld/HelloWorldGUIApp.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+public class HelloWorldGUIApp {
+	public static void main(String[] args) {
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				JFrame frame = new JFrame("HelloWorld GUI");
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frame.setPreferredSize(new Dimension(400, 300));
+				JLabel label = new JLabel("Hello World!!", SwingConstants.CENTER);
+				frame.getContentPane().add(label);
+				
+//				Toolkit tool= Toolkit.getDefaultToolkit();
+//				Dimension dim= tool.getScreenSize();
+				Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+				frame.setLocation(dim.width/2-400/2, dim.height/2-300/2);
+				
+				frame.pack();
+				frame.setVisible(true);
+			}
+		});
+	}
+}
