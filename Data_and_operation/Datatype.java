180b0e357dc161e85931f7984c6a0ae1fb350ead
wogh0 <wogh0@DESKTOP-OS4S3HI.mshome.net>
2021-11-28 15:45:12 +0900

    ㅇㅇ

---
+++ Data_and_operation/Datatype.java
@@ -0,0 +1,16 @@
+public class Datatype{
+	public static void main(String[] args) {
+		System.out.println(6); // Number
+		System.out.println("six"); // String
+		System.out.println("6"); // String 6
+		
+		System.out.println(6+6); // 12
+		System.out.println("6" + "6"); // 66
+		
+		System.out.println(6*6); // 36
+//		System.out.println("6"*"6"); // 오류
+		
+		System.out.println("1111".length()); // 4 문자열의 길이 o
+//		System.out.println(1111.length()); // 숫자는 길이 없음 -> 오류
+	}
+}
