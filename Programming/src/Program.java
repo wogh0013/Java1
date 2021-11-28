d72b995af30ccdd3fc32b4d5bc1c0269221014d5
wogh0 <wogh0@DESKTOP-OS4S3HI.mshome.net>
2021-11-28 17:01:23 +0900

    ㅇㅇ

---
+++ Programming/src/Program.java
@@ -0,0 +1,12 @@
+
+public class Program {
+	public static void main(String[] args) {
+
+		System.out.println(1);
+		System.out.println(2);
+		System.out.println(3);
+
+	}
+}
+
+// 프로그래밍을 통해 순차적으로 실행하여 자동화 가능 ! 
