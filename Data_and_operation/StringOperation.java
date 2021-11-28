570348044019fce5cc6e5a022afe88e331280ec5
wogh0 <wogh0@DESKTOP-OS4S3HI.mshome.net>
2021-11-28 16:34:22 +0900

    Variable

--- Data_and_operation/StringOperation.java
+++ Data_and_operation/StringOperation.java
@@ -1,6 +1,8 @@
 
 public class StringOperation {
 	public static void main(String[] args) {
-		System.out.println("Hello World".length());
+		System.out.println("Hello World".length()); //공백포함 문자열 길이
+		
+		System.out.println("Hello, [name] ... bye!".replace("[name]", "jaeho"));
 	}
 }
---
+++ Data_and_operation/Variable.java
@@ -0,0 +1,19 @@
+
+public class Variable {
+	public static void main(String[] args) {
+		
+		int a = 1; // Number -> integer ... -2, -1, 0, 1, 2 ...
+		System.out.println(a); // 1 출력
+		
+		double b = 1.1; // real number -> double ... -2.0, -1.0, 0, 1.0, 2.0
+		System.out.println(b); // 1.1 출력
+		
+		String c = "Hello World"; // 문자열
+		System.out.println(c);
+	}
+}
+
+
+// 변수의 데이터 타입 - 필요한 값을 꺼낼 때마다 데이터 타입을 확인하지 않아도 돼서 편함 !
+// 일반 물컵 안에 있는 물 - 어떤 물인지 몰라서 마실 수 x
+// 미개봉 물병 안에 있는 물 - 안심하고 마실 수 o
